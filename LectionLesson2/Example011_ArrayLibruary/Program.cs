67a4d1a95b120bd7cfff24e06cd7dd1f86570eea
Alex <16alextancura@gmail.com>
2023-07-09 22:12:21 +0300

    добавил Example011 и выполнил в нем задачу

---
+++ LectionLesson2/Example011_ArrayLibruary/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        // index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int Index0f(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] Array = new int[10];
+
+FillArray(Array);
+PrintArray(Array);
+Array[4] = 4;
+Array[6] = 4;
+Console.WriteLine();
+
+int pos = Index0f(Array, 4);
+Console.WriteLine(pos);
